e4e13b98b46171023aed71bdf69a5d4d16369442
sravbond <71189579+sravbond@users.noreply.github.com>
2020-09-12 14:36:34 -0400

    Update download.sh
    added link to download background data too

--- download.sh
+++ download.sh
@@ -1,1 +1,4 @@
+#poor local data
 wget https://alarmcom-my.sharepoint.com/:u:/g/personal/sbondugula_objectvideo_com/EfhyP4A7aDBPq3VWqOqRCY4BenZiB15BbGldgbRtF2gouQ?e=eYYVGP
+#background data
+wget https://alarmcom-my.sharepoint.com/:u:/g/personal/sbondugula_objectvideo_com/ETglG2im3JxIiZvcLhcQUW4B3xb1Ep09y1wj0SRA86OA3Q?e=WOQQQT
