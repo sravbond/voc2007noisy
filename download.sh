8ae61d2860375b0d3b327a45fa05940ab6c14648
sravbond <71189579+sravbond@users.noreply.github.com>
2020-09-12 14:40:11 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #poor local data
-wget https://alarmcom-my.sharepoint.com/:u:/g/personal/sbondugula_objectvideo_com/EfhyP4A7aDBPq3VWqOqRCY4BenZiB15BbGldgbRtF2gouQ?e=eYYVGP
+wget https://alarmcom-my.sharepoint.com/:u:/g/personal/sbondugula_objectvideo_com/EfhyP4A7aDBPq3VWqOqRCY4B_TiyomG77E9EVFZzMf8G7Q
 #background data
-wget https://alarmcom-my.sharepoint.com/:u:/g/personal/sbondugula_objectvideo_com/ETglG2im3JxIiZvcLhcQUW4B3xb1Ep09y1wj0SRA86OA3Q?e=WOQQQT
+wget https://alarmcom-my.sharepoint.com/:u:/g/personal/sbondugula_objectvideo_com/ETglG2im3JxIiZvcLhcQUW4B3xb1Ep09y1wj0SRA86OA3Q
